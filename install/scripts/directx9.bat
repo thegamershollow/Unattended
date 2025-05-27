1d94d2ce614bee93ff0f05f18ab8961fa7dd8d5b
thegamershollow <thegamershollow@outlook.com>
2025-05-27 19:37:41 +0000

    added two new batch files to the scripts folder

---
+++ install/scripts/directx9.bat
@@ -0,0 +1,15 @@
+:: OPTIONAL: Install DirectX 9.0c
+:: URL|ALL|https://archive.org/download/windows-xp-updates/Windows%20XP/Microsoft%20DirectX%209.0C%20%28June%202010%29%20Redistributable/directx_Jun2010_redist.exe|packages/directx9/directx_Jun2010_redist.exe
+
+@Echo off
+
+setlocal
+
+set "dxfile=%SystemRoot%\System32\d3dx9_43.dll"
+
+if exist "%dxfile%" goto done
+if "%dxver%"=="4.09.00.0904" goto done
+
+todo.pl " /qn /i %Z%\packages\directx9\directx_Jun2010_redist.exe"
+
+:done
---
+++ install/scripts/legacyupdate.bat
@@ -0,0 +1,8 @@
+:: OPTIONAL: Install Legacy Update and windows xp patch
+::URL|ALL|https://content.legacyupdate.net/legacyupdate/LegacyUpdate.exe|packages/legacyupdate/leagacyupdate.exe
+
+@Echo off
+
+if "%WINVER%" == "winxpsp3" reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\legacyupdate.net" /v "*" /t REG_DWORD /d 2 /f
+
+todo.pl "%Z%\packages\legacyupdate\legacyupdate.exe /S /passive /norestart"
