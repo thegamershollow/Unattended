16f02d06baeb335734bf264b26d558a644e80ed1
thegamershollow <thegamershollow@outlook.com>
2025-05-27 19:42:58 +0000

    deleted more batch files

--- install/scripts/dotnet-win7sp1.bat
+++
@@ -1,5 +0,0 @@
-:: INSTALL :: Microsoft .NET Package 
-:: MORE INFO: http://msdn.microsoft.com/en-us/library/bb822049.aspx
-:: URL|ALL|https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe|updates/common/ndp48-x86-x64-allos-enu.exe
-
-todo.pl ".reboot-on 194 %Z%\updates\common\ndp48-x86-x64-allos-enu.exe /norestart /passive"
