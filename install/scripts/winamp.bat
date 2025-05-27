a4e6a15ab2c6f7e73c09ced8736fe7d5f5abc9e6
thegamershollow <thegamershollow@outlook.com>
2025-05-27 20:32:15 +0000

    fix syntax error

--- install/scripts/winamp.bat
+++ install/scripts/winamp.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL: Install WinAmp
 :: HOME: http://www.winamp.com/
-:: URL|ALL|hhttp://web.archive.org/web/20210902231517/http://www.winamparchive.org/dl/winamp524_full_emusic-7plus.exe|packages/winamp/winamp.exe
+:: URL|ALL|http://web.archive.org/web/20210902231517/http://www.winamparchive.org/dl/winamp524_full_emusic-7plus.exe|packages/winamp/winamp.exe
 
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
 todo.pl "%Z%\packages\winamp\winamp.exe /S /install=SFQR"
