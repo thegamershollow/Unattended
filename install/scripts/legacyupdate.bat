44a5b9000f8187ae0019697f2e09a91996e17505
thegamershollow <thegamershollow@outlook.com>
2025-05-28 23:30:29 +0000

    fix error in script

--- install/scripts/legacyupdate.bat
+++ install/scripts/legacyupdate.bat
@@ -5,4 +5,4 @@
 
 if "%WINVER%" == "winxpsp3" reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\legacyupdate.net" /v "*" /t REG_DWORD /d 2 /f
 
-todo.pl "%Z%\packages\legacyupdate\legacyupdate.exe /S /passive /norestart"
+todo.pl "%Z%\packages\legacyupdate\leagacyupdate.exe /S /passive /norestart"
