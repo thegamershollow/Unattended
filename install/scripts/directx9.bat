7cd596292326b344e8014f9f2cbf4428bbbd0a74
thegamershollow <thegamershollow@outlook.com>
2025-05-28 23:30:18 +0000

    fix error in script

--- install/scripts/directx9.bat
+++ install/scripts/directx9.bat
@@ -10,6 +10,6 @@ set "dxfile=%SystemRoot%\System32\d3dx9_43.dll"
 if exist "%dxfile%" goto done
 if "%dxver%"=="4.09.00.0904" goto done
 
-todo.pl " /qn /i %Z%\packages\directx9\directx_Jun2010_redist.exe"
+todo.pl "%Z%\packages\directx9\directx_Jun2010_redist.exe"
 
 :done
