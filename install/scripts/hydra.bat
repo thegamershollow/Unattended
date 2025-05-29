9947d6d3a2b70d0507479023fa05de7846557db7
thegamershollow <thegamershollow@outlook.com>
2025-05-29 14:38:21 +0000

    added hydra installer

---
+++ install/scripts/hydra.bat
@@ -0,0 +1,4 @@
+:: OPTIONAL: Install hydra, a fork of palemoon to work on Windows XP
+:: URL|ALL|https://github.com/Eclipse-Community/UXP/releases/download/v2025.04.28/hydra-2025.04.28.win32.installer.exe|packages/hydra/hydra.exe
+
+todo.pl "%Z%\packages\hydra\hydra.exe /S"
